d25950f298f0c8eb2894f21f193cb01fbad06198
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-18 19:12:01 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -33,7 +33,7 @@ def select_series_title_with_most_human_characters
   join series on books.series_id = series.id
   where characters.species = 'human'
   order by count(character.species);"
-  
+
 end
 
 def select_character_names_and_number_of_books_they_are_in
