81b12e800fd6663e34548697ac6e2e1a628737a1
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-18 19:20:44 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,7 +31,6 @@ def select_series_title_with_most_human_characters
   inner join characters on character_books.character_id = characters.id
   join books on character_books.book_id = books.id
   join series on books.series_id = series.id
-  where characters.species = 'human'
   order by count(characters.species);"
 
 end
