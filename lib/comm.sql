038f69ee0bade4f1ecb8b57abc55f89ff2ea11fa
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-18 19:09:25 +0000

    Automatically backed up by Learn

--- lib/comm.sql
+++ lib/comm.sql
@@ -2,4 +2,4 @@ select series.title from character_books
 inner join characters on character_books.character_id = characters.id
 join books on character_books.book_id = books.id
 join series on books.series_id = series.id
-group by characters.species;
+where characters.species = 'humans';
